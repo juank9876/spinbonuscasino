e288e7b9c69d62b4681b11102e72e2a9c2c22cda
Juan Carlos Saugar <juank98761@gmail.com>
2025-07-17 14:25:27 +0200

    update

--- src/components/juankui/brandlisty-widget.tsx
+++ src/components/juankui/brandlisty-widget.tsx
@@ -8,7 +8,7 @@ interface Props {
   boton?: string
   limit?: string
 }
-function removeUniversalReset (cssString: string) {
+function removeUniversalReset(cssString: string) {
   // Regex para detectar la regla universal * con esos 3 estilos en cualquier orden y con espacios o saltos
   //const universalRuleRegex = /\*\s*\{\s*(?:box-sizing:\s*border-box;\s*)?(?:margin:\s*0;\s*)?(?:padding:\s*0;\s*)?(?:box-sizing:\s*border-box;\s*)?(?:margin:\s*0;\s*)?(?:padding:\s*0;\s*)?\}/gi
 
@@ -19,7 +19,7 @@ function removeUniversalReset (cssString: string) {
   return cssString.replace(exactRuleRegex, '')
 }
 
-export default function BrandlistyWidget ({
+export default function BrandlistyWidget({
   apiKey,
   listId,
   boton = "Visit now",
@@ -59,23 +59,24 @@ export default function BrandlistyWidget ({
   }, [apiKey, listId, boton, limit])
 
   return (
-    <div className="isolation-isolate relative flex w-full flex-col overflow-auto rounded border bg-white p-4 shadow"
-      style={{ height: 600 }}>
+    <div className=" relative flex w-full flex-col overflow-auto rounded border bg-white p-4 shadow"
+      style={{ height: 800 }}>
 
       {error && <p className="text-sm text-red-600">{error}</p>}
 
       {!error && (
         <div
           ref={ref}
-          className="brandlisty-widget-container max-w-full overflow-auto break-words"
+
+          className="external-casino-list-container max-w-full overflow-auto break-words"
           dangerouslySetInnerHTML={{ __html: html }}
         />
 
       )}
-      <style jsx global>{`
+      <style >{`
         .external-casino-list-container {
           width: 100%;
-          overflow: hidden;
+          
           border-radius: 0.75rem;
         }
         
@@ -87,43 +88,10 @@ export default function BrandlistyWidget ({
           max-width: 100%;
           height: auto;
         }
-          body {
+        body {
           padding: 0px !important
-          }
+        }
       `}</style>
     </div>
   )
 }
-
-
-
-/*
-interface Props {
-  apiKey: string
-  listId: string
-  boton?: string
-  limit?: string
-  height?: number
-}
-
-export default async function BrandlistyWidget ({
-  apiKey,
-  listId,
-  boton = "Visit now",
-  limit = "8",
-  height = 800,
-}: Props) {
-  const src = `https://app.brandlisty.com/nowpcms.php?apikey=${encodeURIComponent(apiKey)}&hash=${encodeURIComponent(listId)}&boton=${encodeURIComponent(boton)}&limit=${encodeURIComponent(limit)}`
-
-  return (
-    <iframe
-      src={src}
-      loading="lazy"
-      className="w-full rounded border shadow"
-      style={{ height }}
-      title="Brandlisty Widget"
-
-    />
-  )
-}
-*/
