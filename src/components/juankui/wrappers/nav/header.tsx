cd08c85c7690da25a0fe99be27d9606ac0730809
Juan Carlos Saugar <juank98761@gmail.com>
2025-09-26 15:37:02 +0200

    fix

--- src/components/juankui/wrappers/nav/header.tsx
+++ src/components/juankui/wrappers/nav/header.tsx
@@ -1,47 +1,35 @@
 //import menu from '@/lib/menu.json'
-import { NavigationMenu } from '@/components/ui/navigation-menu'
-import { fetchAllSlugs, fetchCategories, fetchMenu } from '@/api-fetcher/fetcher'
-import { normalizeUrl } from '@/lib/utils'
-import { contextSiteSettings } from '@/app/context/getSiteSettings'
+
 import { Logo } from './logo'
 import { RenderMenu } from './render-menu'
 import { NavMobile } from './nav-mobile'
 import { ClientHeaderWrapper } from './client-header-wrapper'
+import { Category, NavItemType, SiteSettings, Slug } from '@/types/types'
 
 
-export async function Header() {
-  const rawNavItems = await fetchMenu()
-  const navItems = rawNavItems.filter(item => item.status === 'active')
-  const sortedItems = navItems.sort((a, b) => Number(a.sort_order) - Number(b.sort_order))
-  const allSlugs = await fetchAllSlugs("category")
+export interface HeaderData {
+  normalizedItems: NavItemType[];
+  allSlugs: Slug[];
+  categoriesItems: Category[];
+  settings: SiteSettings;
+}
 
-  const normalizedItems = sortedItems.map(item => ({
-    ...item,
-    url: normalizeUrl(item.url)
-  }))
 
-  const categoriesItems = await fetchCategories()
-  const settings = await contextSiteSettings()
+export async function Header({ normalizedItems, allSlugs, categoriesItems, settings }: HeaderData) {
+  const navProps = { categoriesItems, settings, normalizedItems };
 
-  const navProps = { categoriesItems, settings, normalizedItems }
   return (
     <ClientHeaderWrapper>
-
       <div className="w-custom mx-auto flex h-full flex-row items-center justify-between">
-        <Logo
-          {...settings}
-        />
+        <Logo {...settings} />
 
-        <div className='hidden lg:flex'>
-          <RenderMenu
-            normalizedItems={normalizedItems}
-            allSlugs={allSlugs}
-          />
+        <div className="hidden lg:flex">
+          <RenderMenu normalizedItems={normalizedItems} allSlugs={allSlugs} />
         </div>
+
         {/* VERSION MOVIL */}
         <NavMobile {...navProps} />
       </div>
-
     </ClientHeaderWrapper>
-  )
+  );
 }
