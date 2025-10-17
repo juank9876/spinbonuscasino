e15bf53bcc9a7d7541e1f8c8750311fdb91797d3
Juan Carlos Saugar <juank98761@gmail.com>
2025-10-17 15:46:42 +0200

    fix

--- src/components/juankui/sidebar-with-posts/brandlisty-lite.tsx
+++ src/components/juankui/sidebar-with-posts/brandlisty-lite.tsx
@@ -7,10 +7,11 @@ export function BrandlistyLite() {
     const apiKey = process.env.BRANDLISTY_API_KEY || ''
     const listId = process.env.BRANDLISTY_LIST_ID || ''
     const boton = "Visit now"
-    const limit = "10"
+    const limit = "5"
 
+    // max-h-[900px] max-w-[400px] overflow-x-auto
     return (
-        <aside className={`bg-white rounded-xl h-fit border border-gray-200 overflow-hidden shadow-sm top-20`}>
+        <aside className={`bg-white rounded-xl h-fit border border-gray-200 shadow-sm top-20 flex flex-col`}>
             <div className="relative flex items-center gap-2 bg-gradient-to-r from-[var(--color-primary-semi-dark)] to-[var(--color-primary-dark)] px-5 py-2">
                 <DicesIcon className="w-5 h-5 text-white" />
                 <h2 className="text-lg font-bold text-white padding-none">Brandlisty</h2>
