2fffe54ebaac40a462357d3b70e4fa95e5769cb6
juancarlossa <juank987610@gmail.com>
2025-10-22 14:41:50 +0200

    fix

--- src/app/(debug-pages)/yourip/page.tsx
+++ src/app/(debug-pages)/yourip/page.tsx
@@ -5,7 +5,7 @@ export default async function Page () {
   const countryCode = headerslist.get('x-user-country') || 'WW'
   const ip = headerslist.get('x-real-ip') || headerslist.get('x-forwarded-for') || 'unknown';
   return (
-    <div>
+    <div className="flex flex-1 flex-col items-center justify-center">
       <p>Country Code: {countryCode}</p>
       <p>IP Address: {ip}</p>
     </div>
