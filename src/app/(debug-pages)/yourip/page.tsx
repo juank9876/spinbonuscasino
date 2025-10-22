e314b1fb866c02a573c6d0addd30faffef30fac4
juancarlossa <juank987610@gmail.com>
2025-10-22 15:04:40 +0200

    fix

--- src/app/(debug-pages)/yourip/page.tsx
+++ src/app/(debug-pages)/yourip/page.tsx
@@ -4,14 +4,12 @@ export default async function Page () {
   const headerslist = await headers();
   const countryCode = headerslist.get('x-user-country') || 'WW'
   const ip1 = headerslist.get("cf-connecting-ip")
-  const ip2 = headerslist.get("true-client-ip")
-  const ip3 = headerslist.get("x-forwarded-for")
+    || headerslist.get("true-client-ip")
+    || headerslist.get("x-forwarded-for")
   return (
     <div className="flex flex-1 flex-col items-center justify-center">
       <p>Country Code: {countryCode}</p>
-      <p>cf-connecting-ip {ip1}</p>
-      <p>true-client-ip {ip2}</p>
-      <p>x-forwarded-for {ip3}</p>
+      <p>YourIP: {ip1}</p>
     </div>
   )
 }
